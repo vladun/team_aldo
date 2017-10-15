9c92c6002dbf39386dbfaddb6b94a9bfe366d634
vladun <vladun@gmail.com>
2017-10-14 20:59:32 -0500

    initial more

--- FoundPetBot/views.py
+++ FoundPetBot/views.py
@@ -4,3 +4,12 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+
+def index(request):
+    # Get an HttpRequest - the request parameter
+    # perform operations using information from the request.
+    # Return HttpResponse
+    return HttpResponse('Hello from Django!')
